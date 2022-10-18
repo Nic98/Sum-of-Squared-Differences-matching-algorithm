5de6eb339560bbb25f9ae59ddea932db86af14c0
hans <zhangzihang990715@gmail.com>
2022-10-19 00:29:41 +0800

    fixed bug for ZNCC: image mean

--- NCC.py
+++ NCC.py
@@ -92,9 +92,9 @@ def Z_NCC(left_img, right_img, window_size=9):
                     right_window = np.ascontiguousarray(
                         right_img[window_height[0]:window_height[1], right_window_width[0]:right_window_width[1]])
                     n = (window_width[1] - window_width[0]) * (window_height[1] - window_height[0])
-                    left_mean = np.mean(left_window)
-                    right_mean = np.mean(right_window)
-                    ncc_score = np.sum(np.multiply(left_window - np.mean(left_window), right_window - np.mean(right_window)))
+                    left_mean = np.mean(left_img)
+                    right_mean = np.mean(right_img)
+                    ncc_score = np.sum(np.multiply(left_window - left_mean, right_window - right_mean))
                     ncc_score = ncc_score / (np.sqrt(np.sum((left_window - left_mean)**2)) * np.sqrt(np.sum((right_window - right_mean)**2)))
                     if ncc_score > score:
                         score = ncc_score
