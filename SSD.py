e51f4b2f7b2ad509e9b61f959587ee381ff5a0a9
Nic98 <69286762+Nic98@users.noreply.github.com>
2022-10-18 13:20:55 +0800

    Some comments

---
+++ SSD.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def SSD(left_image, right_image, win_size):
+
+    return
--- main.py
+++ main.py
@@ -1,29 +1,28 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import numpy as np
 import cv2
+from SSD import*
 from matplotlib import pyplot as plt
 from numba import njit, prange
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # Ground Truth image
     truth_image = cv2.imread('./Dataset/2018-07-09-16-11-56_2018-07-09-16-11-56-702-disparity.png', -1) / 256
+    # Image Left
     left_image = cv2.imread('./Dataset/2018-07-09-16-11-56_2018-07-09-16-11-56-702-left.jpg')
+    # Image Right
     right_image = cv2.imread('./Dataset/2018-07-09-16-11-56_2018-07-09-16-11-56-702-right.jpg')
+    # Read the images in Grayscale format
     left_image = cv2.cvtColor(left_image, cv2.COLOR_RGB2GRAY).astype(np.int64)
     right_image = cv2.cvtColor(right_image, cv2.COLOR_RGB2GRAY).astype(np.int64)
 
     plt.subplot(2, 1, 1)
     plt.imshow(left_image, cmap='gray')
+    plt.title("Original Left")
     plt.axis('off')
+
     plt.subplot(2, 1, 2)
     plt.imshow(right_image, cmap='gray')
+    plt.title("Original Right")
     plt.axis('off')
     plt.show()
     pass
