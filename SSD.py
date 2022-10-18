53e68a03418ea2414c826e7f0bd899921abb29dc
Nic98 <69286762+Nic98@users.noreply.github.com>
2022-10-18 21:33:02 +0800

    高斯

--- SSD.py
+++ SSD.py
@@ -1,10 +1,15 @@
 import copy
+
+import cv2
 import numpy as np
 from numba import njit, prange
+import tqdm
+import time
 
 
 @njit(parallel=True)
 def compute_SSD(left_image, right_image, win_width, win_height):
+    # start_time = time.time()
     # Check if two images has the same shape
     if left_image.shape == right_image.shape:
         height, width = left_image.shape
@@ -51,7 +56,81 @@ def compute_SSD(left_image, right_image, win_width, win_height):
                                             w_range[0]:w_range[1]])
             min = 999999
             disparity = 999999
-            for r_x in range(l_x + 1):
+            for r_x in prange(l_x + 1):
+
+                if r_x < left:
+                    pass
+                elif r_x + right + 1 > width:
+                    pass
+                else:
+                    r_w_range = (r_x - left, r_x + right + 1)
+
+                    right_win = np.ascontiguousarray(right_image[
+                                                     h_range[0]:h_range[1],
+                                                     r_w_range[0]:r_w_range[1]])
+
+                    dif = np.sum((left_win - right_win) ** 2)
+
+                    if dif < min:
+                        min = dif
+                        disparity = np.abs(l_x - r_x)
+
+            depth[y, l_x] = disparity
+    # print("--- %s seconds ---" % (time.time() - start_time))
+    return depth
+
+
+def mask_Gaussian(left_image, right_image, win_width, win_height):
+    # Check if two images has the same shape
+    if left_image.shape == right_image.shape:
+        height, width = left_image.shape
+    else:
+        raise Exception("Two images have different shape")
+    print("height:", height, "width:", width)
+
+    # Depth (disparity) map
+    depth = np.zeros((height, width), np.uint8)
+    for i, j in np.ndindex(depth.shape):
+        # np.fill not supported by numba
+        depth[i, j] = left_image[i, j]
+    depth = depth.astype(np.uint8)
+    # depth = copy.deepcopy(left_image)
+
+    # half of the window size
+    half_w = win_width // 2
+    half_h = win_height // 2
+
+    for y in prange(height):
+
+        top = half_h
+        bottom = half_h
+        # Out of bound
+        if y < half_h:
+            top = y
+        if y + half_h > height:
+            bottom = height - y - 1
+        h_range = (y - top, y + bottom + 1)
+
+        for l_x in prange(width):
+
+            left = half_w
+            right = half_w
+            # Out of bound
+            if l_x < half_w:
+                left = l_x
+            if l_x + half_w >= width:
+                right = width - l_x - 1
+
+            w_range = (l_x - left, l_x + right + 1)
+            left_win = np.ascontiguousarray(left_image[
+                                            h_range[0]:h_range[1],
+                                            w_range[0]:w_range[1]])
+            mask = cv2.getGaussianKernel(left_win.shape[0], left_win.shape[1])
+            left_win = left_win * mask
+
+            min = 999999
+            disparity = 999999
+            for r_x in prange(l_x + 1):
 
                 if r_x < left:
                     pass
@@ -70,5 +149,4 @@ def compute_SSD(left_image, right_image, win_width, win_height):
                         disparity = np.abs(l_x - r_x)
 
             depth[y, l_x] = disparity
-
     return depth
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import numpy as np
 import cv2
-from SSD import*
-from evaluation import*
+from SSD import *
+from evaluation import *
 from matplotlib import pyplot as plt
 from numba import njit, prange
+import time
 
 if __name__ == '__main__':
     # Ground Truth image
@@ -16,11 +17,21 @@ if __name__ == '__main__':
     left_image = cv2.cvtColor(left_image, cv2.COLOR_RGB2GRAY).astype(np.int32)
     right_image = cv2.cvtColor(right_image, cv2.COLOR_RGB2GRAY).astype(np.int32)
 
-
-    small_disparity = compute_SSD(left_image, right_image, 10, 10)
-    large_disparity = compute_SSD(left_image, right_image, 50, 50)
-    disparity = compute_SSD(left_image, right_image, 30, 30)
-    evaluate(disparity, truth_image)
+    '''
+    Try to balance the window size to a reasonable value
+    '''
+    # small_disparity = compute_SSD(left_image, right_image, 10, 10)
+    # large_disparity = compute_SSD(left_image, right_image, 50, 50)
+    # disparity = compute_SSD(left_image, right_image, 30, 30)
+    # evaluate(small_disparity, truth_image)
+    # evaluate(large_disparity, truth_image)
+    # evaluate(disparity, truth_image)
+    '''
+    Using a Gaussian kernel on the window to emphasize on the
+    center pixel.
+    '''
+    # blur_disparity = mask_Gaussian(left_image, right_image, 10, 10)
+    # evaluate(blur_disparity, truth_image)
 
     # plt.subplot(3, 1, 1)
     # plt.imshow(left_image, cmap='gray')
@@ -34,9 +45,8 @@ if __name__ == '__main__':
     # plt.show()
 
     plt.subplot(3, 1, 3)
-    plt.imshow(disparity, cmap='gray')
+    plt.imshow(blur_disparity, cmap='gray')
     plt.title("disparity")
     plt.axis('off')
     plt.show()
     pass
-
